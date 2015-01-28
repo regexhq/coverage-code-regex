c29ab48e527e4e3ab9019342dc5d1ea43f26ed43
tunnckoCore <mameto_100@mail.bg>
2015-01-28 05:23:37 +0200

    update regex

--- index.js
+++ index.js
@@ -8,5 +8,6 @@
 'use strict';
 
 module.exports = function coverageCodeRegex() {
-  return /(?:__cov_(?:[\w\W\S.,$]{1,22})\.(?:.{1})\[\'(?:\d{1,})\'\]\+\+\;)/;
+  // e.g. __cov_abc.s['12'][0][2][1]
+  return /(?:__cov_(?:[\w\W\S.,$]{1,22})\.(?:.{1})\[\'(?:\d{1,})\'\](?:\[(?:\d{1,})\])*?\+\+\;)/;
 };
